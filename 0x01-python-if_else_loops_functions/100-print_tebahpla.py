43d531d8bfb0cdb4a88df2f6714d38e650307eb4
ianotee <oteeian500@gmail.com>
2023-09-05 13:10:43 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+m = 0
+for w in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(w - m)), end="")
+    m = 32 if m == 0 else 0
 
