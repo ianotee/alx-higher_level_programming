9482f49f21d368f9cb4b65933666ec1e5fe8dbc3
ianotee <oteeian500@gmail.com>
2023-09-07 06:54:00 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Prints the names."""
+    """Print all names."""
     import hidden_4
 
-    naun = dir(hidden_4)
-    for naun in names:
-        if naun[:2] != "__":
-            print(naun)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
