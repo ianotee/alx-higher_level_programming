dac179cb94c90e879f5aeb62de9cb132a798f15d
ianotee <oteeian500@gmail.com>
2023-09-25 04:44:30 -0700

    fourth

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-"""This prints both integers and strings"""
 def safe_print_list(my_list=[], x=0):
-    sam = 0
+    total = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
-            sam += 1
+            total += 1
         except IndexError:
             break
     print()
-    return(sam)
+    return(total)
