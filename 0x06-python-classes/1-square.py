7db4834b4b5704b9190a475cab40eb20b600b114
ianotee <oteeian500@gmail.com>
2023-09-26 04:49:29 -0700

    third

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This is  a square class """
+
+
+class Square:
+    """This is a square"""
+
+    def __init__(self, size):
+        """The initialization of an instance/object variable"""
+
+
+        self.__size = size
