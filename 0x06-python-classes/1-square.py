919c43bb87b6c461dc960787e0a69ac03f5d630c
ianotee <oteeian500@gmail.com>
2023-09-26 16:23:47 -0700

    16

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-# 0-square.py
-"""This is  a square class """
+""" The class"""
 
 
 class Square:
-    """This is a square"""
+    """The square"""
 
     def __init__(self, size):
-        """The initialization of an instance/object variable
-         size - The size  of the square.
-        """
-
 
         self.__size = size
