37284cae4d567d28fc5c06cb46040a18401b7ee0
ianotee <oteeian500@gmail.com>
2023-11-14 04:43:41 -0800

    third commit

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 --It will print the  description of the tables.
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
 SHOW CREATE TABLE first_table;
