f557c72d5abef302115f666d7cf8c529a17e1e72
ianotee <oteeian500@gmail.com>
2023-12-12 10:30:41 -0800

    fifth commit

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
+
 const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
+
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+}
 
-  double () {
-    super.double();
-  }
-};
+module.exports = Square;
