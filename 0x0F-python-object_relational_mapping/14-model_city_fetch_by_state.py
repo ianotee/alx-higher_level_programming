297110bfc3d7948eb8f61f278c7de7e61a080697
ianotee <oteeian500@gmail.com>
2023-12-17 13:56:27 -0800

    tenth commit

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 """
-This script is used to print the city objects .
+The script prints all City objects.
 """
 
 from sys import argv
-from model_state import State, Base
+from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Access the objects from the database.
+    db access.
     """
-
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-
-    engine = create_engine(db_url)
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    results = session.query(City, State).join(State)
+    query = session.query(City, State).join(State)
 
-    for city, state in results.all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for _c, _s in query.all():
+        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
 
     session.commit()
     session.close()
