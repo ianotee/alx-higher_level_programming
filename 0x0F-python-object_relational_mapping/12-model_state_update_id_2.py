bb688e1d4c5109399448591891ae5bc393c48f68
ianotee <oteeian500@gmail.com>
2023-12-17 13:48:07 -0800

    eith commit

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 """
-This script is used to change the objects in the database.
+The script changes name of state objects.
 """
-
 from sys import argv
-from model_state import State, Base
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    This code is used to update objects in the database.
-    """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-
-    engine = create_engine(db_url)
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
-
     session = Session()
-
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = "New Mexico"
+    ari_state = session.query(State).filter(State.id == '2').first()
+    ari_state.name = 'New Mexico'
     session.commit()
-
     session.close()
