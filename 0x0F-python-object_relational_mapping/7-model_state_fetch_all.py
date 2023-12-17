4049618d35b320e4bf05fc4ab6436e13514bc55c
ianotee <oteeian500@gmail.com>
2023-12-17 13:23:59 -0800

    second commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """
-This will display the list of all the cities.
+script that lists all State objects from the database hbtn_0e_6_u.
 """
 
-from sys import argv
-from model_state import State, Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
-if __name__ == "__main__":
-    """
-    The database.
-    """
-
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
-
-    engine = create_engine(db_url)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
+    s = Session()
 
-    session = Session()
-
-    for instance in session.query(State).order_by(State.id):
-        print('{0}: {1}'.format(instance.id, instance.name))
+    for row in s.query(State).order_by(State.id.asc()):
+        print(row.id, end="")
+        print(': ', end="")
+        print(row.name)
