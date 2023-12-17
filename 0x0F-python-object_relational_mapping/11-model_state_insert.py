887d5d4952b6409f31e35a762f2a53827934cb06
ianotee <oteeian500@gmail.com>
2023-12-17 13:44:37 -0800

    seventh commit

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """
-This is used to add states to the database.
+script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+    - script takes 3 arguments: mysql username, mysql password and databasename
+    - uses the module SQLAlchemy
+    - imports State and Base from model_state
+    - script should connect to a MySQL server running on localhost at port 3306
+    Print the new states.id after creation
 """
 
-from sys import argv
-from model_state import State, Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
-if __name__ == "__main__":
-    """
-    Is used to get data of the states.
-    """
-
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
-
-    engine = create_engine(db_url)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
-
-    session = Session()
+    s = Session()
 
     new_state = State(name="Louisiana")
-    session.add(new_state)
-    session.commit()
-
-    print('{0}'.format(new_state.id))
-    session.close()
+    s.add(new_state)
+    s.commit()
+    print(new_state.id)
